cbfa6dd2849377eff19780041d7c7baf1bb1a82f
diop <fodesdiop@gmail.com>
2020-05-08 01:01:30 +0000

    Add Contrast, Brightness and Blur

--- app.py
+++ app.py
@@ -34,12 +34,30 @@ def main():
         enhance_type = st.sidebar.radio('Enhance Type', ['Original', 'Grayscale', 'Contrast', 'Brightness', 'Blur'])
         if enhance_type == 'Grayscale':
             new_img = np.array(uploaded.convert('RGB'))
-            mid_img = cv2.cvtColor(new_img, 1)
-            gray = cv2.cvtColor(mid_img, cv2.COLOR_BGR2GRAY)
+            temp_img = cv2.cvtColor(new_img, 1)
+            gray = cv2.cvtColor(temp_img, cv2.COLOR_BGR2GRAY)
             st.image(gray)
             # st.write(gray)
             # st.write(new_img)
 
+        if enhance_type == 'Contrast':
+            contrast_rate = st.sidebar.slider('Contrtast', 0.5, 3.5)
+            enhancer = ImageEnhance.Contrast(uploaded)
+            img_output = enhancer.enhance(contrast_rate)
+            st.image(img_output)
+
+        if enhance_type == 'Brightness':
+            contrast_rate = st.sidebar.slider('Brigthness', 0.5, 3.5)
+            enhancer = ImageEnhance.Brightness(uploaded)
+            img_output = enhancer.enhance(contrast_rate)
+            st.image(img_output)
+
+        if enhance_type == 'Blur':
+            blur_rate = st.sidebar.slider('Blur', 0.5, 3.5)
+            new_img = np.array(uploaded.convert('RGB'))
+            temp_img = cv2.cvtColor(new_img, 1)
+            blurred = cv2.GaussianBlur(temp_img, (11,11), blur_rate)
+            st.image(blurred)
 
     elif choice == 'About':
         st.subheader('About')
