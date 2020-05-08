61a6e591bb665537d0aa29c13a9f47af22f11151
diop <fodesdiop@gmail.com>
2020-05-08 00:07:27 +0000

    Add grayscale

--- app.py
+++ app.py
@@ -27,8 +27,19 @@ def main():
 
         if image_file is not None:
             uploaded = Image.open(image_file)
+            # st.write(type(uploaded))
             st.text('Original Image')
             st.image(uploaded)
+
+        enhance_type = st.sidebar.radio('Enhance Type', ['Original', 'Grayscale', 'Contrast', 'Brightness', 'Blur'])
+        if enhance_type == 'Grayscale':
+            new_img = np.array(uploaded.convert('RGB'))
+            mid_img = cv2.cvtColor(new_img, 1)
+            gray = cv2.cvtColor(mid_img, cv2.COLOR_BGR2GRAY)
+            st.image(gray)
+            # st.write(gray)
+            # st.write(new_img)
+
 
     elif choice == 'About':
         st.subheader('About')
